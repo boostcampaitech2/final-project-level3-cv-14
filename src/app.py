272a7f3236106890a9d32627c24887488d196dd1
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-12-09 05:31:42 +0000

    fix: app.py using FastAPI - SuperResolution Inference

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
 from PIL import Image
-import torch
+import PIL
 import cv2
-import argparse
-import os
+import torch
 import tensorflow as tf
+import os
+import argparse
+import streamlit as st
+import io
+import base64
+import requests
 from SuperRes.predict import Predictor as sr_predictor
 from Deblur.predict import Predictor as db_predictor
 
@@ -29,9 +33,21 @@ def main():
       image = Image.open(uploaded_file1)
       st.image(image, caption='Original Image', use_column_width=False)
       st.write("")
-      new_image = sr_predictor().predict(image) #TODO: sidebar에서 선택한 args 넘겨주기
+      #TODO: image crop, resize scale 선택
+      #TODO: sidebar에서 선택한 args 넘겨주기
+
+      ###기존 함수 추론 start ###
+      #new_image = sr_predictor().predict(image) 
+      ###기존 함수 추론 end ###
+      
+      ### Fast API 추론 start ###
+      files = {'files':uploaded_file1.getvalue()}
+      response = requests.post('http://127.0.0.1:8000/super',files=files)
+      bytes_data = io.BytesIO(response.content)
+      new_image = Image.open(bytes_data)
+      ### Fast API 추론 end ###
+
       st.image(new_image, caption='Processed Image', use_column_width=True)
-      #st.write("(psnr, ssim, psnr_y, ssim_y) = ",score)
 
   ### Deblur ###
   st.title("Deblur")
@@ -42,7 +58,9 @@ def main():
       image = Image.open(uploaded_file2)
       st.image(image, caption='Original Image', use_column_width=False)
       st.write("")
+      #TODO: image crop
       new_image = db_predictor().predict(image)
+      #TODO: image combine
       st.image(new_image, caption='Processed Image', use_column_width=True)
 
 if __name__ == '__main__':
