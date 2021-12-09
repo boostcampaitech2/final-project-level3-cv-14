089d6d0a6a5a786845be45d42459120ced924ee4
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-12-09 06:15:40 +0000

    feature: add sidebar

--- src/app.py
+++ src/app.py
@@ -15,63 +15,46 @@ from SuperRes.predict import Predictor as sr_predictor
 from Deblur.predict import Predictor as db_predictor
 
 def main():
-	#st.title("Awesome Streamlit for MLDDD")
-	#st.subheader("How to run streamlit from colab")
-  st.write("""
-  # 종합 이미지 보정 도구
+  st.title("종합 이미지 보정 도구 ")
+  menu = ['Image Completion','Super Resolution','Deblur']
+  st.sidebar.header('Mode Selection')
+  choice = st.sidebar.selectbox('이미지 보정 도구 선택', menu)
+  uploaded_file = st.sidebar.file_uploader("Choose an image...", type="jpg",key="SR")
+  execute_btn = st.sidebar.button('결과 보기')
 
-  Image completion, Super resolution, Deblur
-  
-  """)
+  if choice=='Image Completion':
+    st.write('image completion')
+  else:
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Original Image', use_column_width=False)
+        st.write("")
 
-  ### Super Resolution ###
-  st.title("Super Resolution")
-  st.write("이미지의 해상도를 높여줍니다.")
+    if execute_btn:
+      ### Super Resolution ###
+      if choice=='Super Resolution':
+        pass
+          #TODO: image crop, resize scale 선택
+          #TODO: sidebar에서 선택한 args 넘겨주기
+          ###기존 함수 추론 start ###
+          #new_image = sr_predictor().predict(image) 
+          ###기존 함수 추론 end ###
 
-  uploaded_file1 = st.file_uploader("Choose an image...", type="jpg",key="SR")
-  if uploaded_file1 is not None:
-      image = Image.open(uploaded_file1)
-      st.image(image, caption='Original Image', use_column_width=False)
-      st.write("")
-      #TODO: image crop, resize scale 선택
-      #TODO: sidebar에서 선택한 args 넘겨주기
+      ### Deblur ###
+      elif choice=='Deblur':
+        pass
+        #TODO: image crop
+        ###기존 함수 추론 start ###
+        #new_image = db_predictor().predict(image)
+        ###기존 함수 추론 end ###
 
-      ###기존 함수 추론 start ###
-      #new_image = sr_predictor().predict(image) 
-      ###기존 함수 추론 end ###
-      
       ### Fast API 추론 start ###
-      files = {'files':uploaded_file1.getvalue()}
+      files = {'files':uploaded_file.getvalue()}
       response = requests.post('http://127.0.0.1:8000/super',files=files) #TODO: change into server addr
       bytes_data = io.BytesIO(response.content)
       new_image = Image.open(bytes_data)
       ### Fast API 추론 end ###
 
-      st.image(new_image, caption='Processed Image', use_column_width=True)
-
-  ### Deblur ###
-  st.title("Deblur")
-  st.write("Blurry 이미지를 보정해줍니다.")
-
-  uploaded_file2 = st.file_uploader("Choose an image...", type="jpg",key="DB")
-  if uploaded_file2 is not None:
-      image = Image.open(uploaded_file2)
-      st.image(image, caption='Original Image', use_column_width=False)
-      st.write("")
-      #TODO: image crop
-
-      ###기존 함수 추론 start ###
-      #new_image = db_predictor().predict(image)
-      ###기존 함수 추론 end ###
-      
-      ### Fast API 추론 start ###
-      files = {'files':uploaded_file2.getvalue()}
-      response = requests.post('http://127.0.0.1:8000/deblur',files=files) #TODO: change into server addr
-      bytes_data = io.BytesIO(response.content)
-      new_image = Image.open(bytes_data)
-      ### Fast API 추론 end ###
-
-      #TODO: image combine
       st.image(new_image, caption='Processed Image', use_column_width=True)
 
 if __name__ == '__main__':
