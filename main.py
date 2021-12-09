e27a5b9c852e9ace5ac138ba10b31593764bad3a
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-12-09 18:33:53 +0000

    remove main.py and fix README

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-import argparse
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train model.")
-    parser.add_argument("--port", default=6006,type=int)
-    args = parser.parse_args()
-
-    os.system('streamlit run WebServer/Server_SRD.py --server.port {}'.format(args.port))
