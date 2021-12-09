02d04ad6f18ef3297ef55bb063a35135deaba970
김범수 <bamtol5430@naver.com>
2021-12-09 17:45:36 +0900

    fix: enable click button after image uploaded

--- src/app.py
+++ src/app.py
@@ -26,10 +26,6 @@ def main():
 
   #파일 선택
   uploaded_file = st.sidebar.file_uploader("Choose an image...", type="jpg",key="SR")
-  if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Original Image', use_column_width=True)
-    st.write("")
   
   #모듈 실행
   ### Image Completion ###
@@ -41,28 +37,39 @@ def main():
   elif choice=='Super Resolution':
     ratio_list = ['2','3','4','8']
     ratio = st.sidebar.selectbox('이미지 확대 비율 선택',ratio_list)
-    #TODO: image crop, resize scale 선택
-    #TODO: sidebar에서 선택한 args 넘겨주기
-    if st.sidebar.button('결과 보기'):
-      with st.spinner('Processing...'):
-        files = {'files':uploaded_file.getvalue()}
-        response = requests.post('http://127.0.0.1:8000/super',files=files) #TODO: change into server addr
-        bytes_data = io.BytesIO(response.content)
-        new_image = Image.open(bytes_data)
-      st.success('Done!')
-      st.image(new_image, caption='Processed Image', use_column_width=True)
+
+    if uploaded_file is not None:
+      image = Image.open(uploaded_file)
+      st.image(image, caption='Original Image', use_column_width=True)
+      st.write("")
+
+      #TODO: image crop, resize scale 선택
+      #TODO: sidebar에서 선택한 args 넘겨주기
+      if st.sidebar.button('결과 보기'):
+        with st.spinner('Processing...'):
+          files = {'files':uploaded_file.getvalue()}
+          response = requests.post('http://127.0.0.1:8000/super',files=files) #TODO: change into server addr
+          bytes_data = io.BytesIO(response.content)
+          new_image = Image.open(bytes_data)
+        st.success('Done!')
+        st.image(new_image, caption='Processed Image', use_column_width=True)
   
   ### Deblur ###
   elif choice=='Deblur':
-    #TODO: image crop
-    if st.sidebar.button('결과 보기'):
-      with st.spinner('Processing...'):
-        files = {'files':uploaded_file.getvalue()}
-        response = requests.post('http://127.0.0.1:8000/deblur',files=files) #TODO: change into server addr
-        bytes_data = io.BytesIO(response.content)
-        new_image = Image.open(bytes_data)
-      st.success('Done!')
-      st.image(new_image, caption='Processed Image', use_column_width=True)
+    if uploaded_file is not None:
+      image = Image.open(uploaded_file)
+      st.image(image, caption='Original Image', use_column_width=True)
+      st.write("")
+
+      #TODO: image crop
+      if st.sidebar.button('결과 보기'):
+        with st.spinner('Processing...'):
+          files = {'files':uploaded_file.getvalue()}
+          response = requests.post('http://127.0.0.1:8000/deblur',files=files) #TODO: change into server addr
+          bytes_data = io.BytesIO(response.content)
+          new_image = Image.open(bytes_data)
+        st.success('Done!')
+        st.image(new_image, caption='Processed Image', use_column_width=True)
 
 if __name__ == '__main__':
 	main()
