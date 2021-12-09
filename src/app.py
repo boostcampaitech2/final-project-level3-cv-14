c393e7cdf2358c63c6f45b5b5127eff51bc41a48
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-12-09 06:32:42 +0000

    feature: sidebar options depending on module

--- src/app.py
+++ src/app.py
@@ -16,45 +16,48 @@ from Deblur.predict import Predictor as db_predictor
 
 def main():
   st.title("종합 이미지 보정 도구 ")
+  st.sidebar.header('Choose Options')
+
+  #module 선택
   menu = ['Image Completion','Super Resolution','Deblur']
-  st.sidebar.header('Mode Selection')
   choice = st.sidebar.selectbox('이미지 보정 도구 선택', menu)
+  if choice:
+    st.subheader(choice)
+
+  #파일 선택
   uploaded_file = st.sidebar.file_uploader("Choose an image...", type="jpg",key="SR")
-  execute_btn = st.sidebar.button('결과 보기')
-
+  if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Original Image', use_column_width=False)
+    st.write("")
+  
+  #모듈 실행
+  ### Image Completion ###
   if choice=='Image Completion':
-    st.write('image completion')
-  else:
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Original Image', use_column_width=False)
-        st.write("")
-
-    if execute_btn:
-      ### Super Resolution ###
-      if choice=='Super Resolution':
-        pass
-          #TODO: image crop, resize scale 선택
-          #TODO: sidebar에서 선택한 args 넘겨주기
-          ###기존 함수 추론 start ###
-          #new_image = sr_predictor().predict(image) 
-          ###기존 함수 추론 end ###
-
-      ### Deblur ###
-      elif choice=='Deblur':
-        pass
-        #TODO: image crop
-        ###기존 함수 추론 start ###
-        #new_image = db_predictor().predict(image)
-        ###기존 함수 추론 end ###
-
-      ### Fast API 추론 start ###
+    pass
+    #TODO: Image Completion 모듈 추가
+  
+  ### Super Resolution ###
+  elif choice=='Super Resolution':
+    ratio_list = ['2','3','4','8']
+    ratio = st.sidebar.selectbox('이미지 확대 비율 선택',ratio_list)
+    #TODO: image crop, resize scale 선택
+    #TODO: sidebar에서 선택한 args 넘겨주기
+    if st.sidebar.button('결과 보기'):
       files = {'files':uploaded_file.getvalue()}
       response = requests.post('http://127.0.0.1:8000/super',files=files) #TODO: change into server addr
       bytes_data = io.BytesIO(response.content)
       new_image = Image.open(bytes_data)
-      ### Fast API 추론 end ###
-
+      st.image(new_image, caption='Processed Image', use_column_width=True)
+  
+  ### Deblur ###
+  elif choice=='Deblur':
+    #TODO: image crop
+    if st.sidebar.button('결과 보기'):
+      files = {'files':uploaded_file.getvalue()}
+      response = requests.post('http://127.0.0.1:8000/super',files=files) #TODO: change into server addr
+      bytes_data = io.BytesIO(response.content)
+      new_image = Image.open(bytes_data)
       st.image(new_image, caption='Processed Image', use_column_width=True)
 
 if __name__ == '__main__':
