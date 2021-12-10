f2fc55618c9421909da867b4d871ae907eb6373b
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-12-10 06:31:30 +0000

    feature: model selection based on expand ratio

--- SuperResolution/Wrapper.py
+++ SuperResolution/Wrapper.py
@@ -17,37 +17,27 @@ class SuperResolution():
             'real_sr': {
                 4: os.path.join(model_dir, '003_realSR_BSRGAN_DFO_s64w8_SwinIR-M_x4_GAN.pth')
             },
-            'gray_dn': {
-                15: os.path.join(model_dir, '004_grayDN_DFWB_s128w8_SwinIR-M_noise15.pth'),
-                25: os.path.join(model_dir, '004_grayDN_DFWB_s128w8_SwinIR-M_noise25.pth'),
-                50: os.path.join(model_dir, '004_grayDN_DFWB_s128w8_SwinIR-M_noise50.pth')
-            },
-            'color_dn': {
-                15: os.path.join(model_dir, '005_colorDN_DFWB_s128w8_SwinIR-M_noise15.pth'),
-                25: os.path.join(model_dir, '005_colorDN_DFWB_s128w8_SwinIR-M_noise25.pth'),
-                50: os.path.join(model_dir, '005_colorDN_DFWB_s128w8_SwinIR-M_noise50.pth')
-            },
-            'jpeg_car': {
-                10: os.path.join(model_dir, '006_CAR_DFWB_s126w7_SwinIR-M_jpeg10.pth'),
-                20: os.path.join(model_dir, '006_CAR_DFWB_s126w7_SwinIR-M_jpeg20.pth'),
-                30: os.path.join(model_dir, '006_CAR_DFWB_s126w7_SwinIR-M_jpeg30.pth'),
-                40: os.path.join(model_dir, '006_CAR_DFWB_s126w7_SwinIR-M_jpeg40.pth')
+            'classical_sr':{
+                2: os.path.join(model_dir, '001_classicalSR_DF2K_s64w8_SwinIR-M_x2.pth'),
+                3: os.path.join(model_dir, '001_classicalSR_DF2K_s64w8_SwinIR-M_x3.pth'),
+                4: os.path.join(model_dir, '001_classicalSR_DF2K_s64w8_SwinIR-M_x4.pth'),
+                8: os.path.join(model_dir, '001_classicalSR_DF2K_s64w8_SwinIR-M_x8.pth'),
             }
         }
 
         parser = argparse.ArgumentParser()
-        parser.add_argument('--task', type=str, default='real_sr', help='classical_sr, lightweight_sr, real_sr, '
+        parser.add_argument('--task', type=str, default='classical_sr', help='classical_sr, lightweight_sr, real_sr, '
                                                                         'gray_dn, color_dn, jpeg_car')
         parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8')  # 1 for dn and jpeg car
         parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
         parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
-        parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
+        parser.add_argument('--training_patch_size', type=int, default=64, help='patch size used in training SwinIR. '
                                                                                  'Just used to differentiate two different settings in Table 2 of the paper. '
                                                                                  'Images are NOT tested patch by patch.')
         parser.add_argument('--large_model', action='store_true',
                             help='use large model, only provided for real image sr')
         parser.add_argument('--model_path', type=str,
-                            default=self.model_zoo['real_sr'][4])
+                            default=self.model_zoo['classical_sr'][4])
         parser.add_argument('--folder_lq', type=str, default=None, help='input low-quality test image folder')
         parser.add_argument('--folder_gt', type=str, default=None, help='input ground-truth test image folder')
 
@@ -63,27 +53,15 @@ class SuperResolution():
         }
 
     @st.cache
-    def predict(self, image, task_type='Real-World Image Super-Resolution', jpeg=40, noise=15,scale=None):
-        self.args.task = self.tasks[task_type]
+    def predict(self, image, task_type='Real-World Image Super-Resolution', jpeg=40, noise=15,scale=4):
+        # self.args.task = self.tasks[task_type]
+        self.args.task = 'classical_sr'
         self.args.noise = noise
         self.args.jpeg = jpeg
 
-        if scale is not None:
-            self.args.scale = scale
-            #TODO: 사용자 선택 scale에 따라 model 선택, args 지정
+        self.args.scale = scale
+        self.args.model_path = self.model_zoo[self.args.task][self.args.scale]
 
-        # set model path
-        if self.args.task == 'real_sr':
-            self.args.scale = 4
-            self.args.model_path = self.model_zoo[self.args.task][4]
-        elif self.args.task in ['gray_dn', 'color_dn']:
-            self.args.model_path = self.model_zoo[self.args.task][noise]
-        else:
-            self.args.model_path = self.model_zoo[self.args.task][jpeg]
-
-        #img_array = np.array(image)
-        #img = image.save('temp.jpg')
-        cv2.imwrite('temp.jpg',image)
         model = define_model(self.args)
         model.eval()
         model = model.to(self.device)
@@ -92,20 +70,20 @@ class SuperResolution():
         _, _, border, window_size = setup(self.args)
         
         # read image
-        imgname, img_lq, img_gt = get_image_pair(self.args, 'temp.jpg')  # image to HWC-BGR, float32
-        img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]],
+        img_gt = cv2.cvtColor(image, cv2.IMREAD_COLOR).astype(np.float32) / 255. # image to HWC-BGR, float32
+        img_gt = np.transpose(img_gt if img_gt.shape[2] == 1 else img_gt[:, :, [2, 1, 0]],
                                 (2, 0, 1))  # HCW-BGR to CHW-RGB
-        img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to(self.device)  # CHW-RGB to NCHW-RGB
+        img_gt = torch.from_numpy(img_gt).float().unsqueeze(0).to(self.device)  # CHW-RGB to NCHW-RGB
 
         # inference
         with torch.no_grad():
             # pad input image to be a multiple of window_size
-            _, _, h_old, w_old = img_lq.size()
+            _, _, h_old, w_old = img_gt.size()
             h_pad = (h_old // window_size + 1) * window_size - h_old
             w_pad = (w_old // window_size + 1) * window_size - w_old
-            img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[:, :, :h_old + h_pad, :]
-            img_lq = torch.cat([img_lq, torch.flip(img_lq, [3])], 3)[:, :, :, :w_old + w_pad]
-            output = model(img_lq)
+            img_gt = torch.cat([img_gt, torch.flip(img_gt, [2])], 2)[:, :, :h_old + h_pad, :]
+            img_gt = torch.cat([img_gt, torch.flip(img_gt, [3])], 3)[:, :, :, :w_old + w_pad]
+            output = model(img_gt)
             output = output[..., :h_old * 4, :w_old * 4]
         output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
         if output.ndim == 3:
