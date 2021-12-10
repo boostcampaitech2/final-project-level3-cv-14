e914d76087aa211f355df4f78c7b15f9382a4dcd
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-12-10 10:14:26 +0000

    add realsr weight dowlnload code

--- SuperResolution/download-weights.sh
+++ SuperResolution/download-weights.sh
@@ -4,3 +4,4 @@ wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/001_classical
 wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/001_classicalSR_DF2K_s64w8_SwinIR-M_x3.pth -P SwinIR/experiments/pretrained_models
 wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/001_classicalSR_DF2K_s64w8_SwinIR-M_x4.pth -P SwinIR/experiments/pretrained_models
 wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/001_classicalSR_DF2K_s64w8_SwinIR-M_x8.pth -P SwinIR/experiments/pretrained_models
+wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/003_realSR_BSRGAN_DFO_s64w8_SwinIR-M_x4_GAN.pth -P SwinIR/experiments/pretrained_models
