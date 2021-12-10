ca4d439b9315a05aadc8b6762c0840be08771865
sronger <kjt8709@nate.com>
2021-12-10 19:04:59 +0000

    add interactive deblur

--- WebServer/Server_SRD.py
+++ WebServer/Server_SRD.py
@@ -12,78 +12,124 @@ import io
 import base64
 import requests
 from streamlit_cropper import st_cropper
+from streamlit_drawable_canvas import st_canvas
 import sys
+import copy
+
 sys.path.append(os.getcwd())
 from Utils import ImageEncoder
 
+
 def main():
-  #st.title("종합 이미지 보정 도구 ")
-  st.sidebar.header('Choose Options')
+    # st.title("종합 이미지 보정 도구 ")
+    st.sidebar.header('Choose Options')
 
-  #module 선택
-  menu = ['Image Completion','Super Resolution','Deblur']
-  choice = st.sidebar.selectbox('이미지 보정 도구 선택', menu)
-  if choice:
-    st.subheader(choice)
+    # module 선택
+    menu = ['Image Completion', 'Super Resolution', 'Deblur']
+    choice = st.sidebar.selectbox('이미지 보정 도구 선택', menu)
+    if choice:
+        st.subheader(choice)
 
-  #파일 선택
-  uploaded_file = st.sidebar.file_uploader("Choose an image...", type="jpg",key="SR")
-  
-  #모듈 실행
-  ### Image Completion ###
-  if choice=='Image Completion':
-    pass
-    #TODO: Image Completion 모듈 추가
-  
-  ### Super Resolution ###
-  elif choice=='Super Resolution':
-    ratio_list = ['2','3','4','8']
-    ratio = st.sidebar.selectbox('이미지 확대 비율 선택',ratio_list)
+    # 파일 선택
+    uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"], key="SR")
 
-    if uploaded_file is not None:
-      image = Image.open(uploaded_file)
-      st.write("")
+    # 모듈 실행
+    ### Image Completion ###
+    if choice == 'Image Completion':
+        pass
+        # TODO: Image Completion 모듈 추가
 
-      # TODO: image crop canvas 수정필요
-      cropped_img = st_cropper(image, aspect_ratio=None)
+    ### Super Resolution ###
+    elif choice == 'Super Resolution':
+        ratio_list = ['2', '3', '4', '8']
+        ratio = st.sidebar.selectbox('이미지 확대 비율 선택', ratio_list)
 
-      if st.sidebar.button('결과 보기'):
-        with st.spinner('Processing...'):
-          image = np.array(cropped_img.convert('RGB'))
-          img_byte_arr = ImageEncoder.Encode(image, ext='jpg', quality=90)
-          files = {
-            'image': img_byte_arr,
-            'ratio': (None, int(ratio)),
-          }
-          response = requests.post('http://127.0.0.1:8000/super',files=files) #TODO: change into server addr
-        if response.status_code==200:
-          new_image = ImageEncoder.Decode(response.content)
-          st.success('Done!')
-          col1, col2 = st.columns(2)
-          col1.image(cropped_img, caption='Cropped Image', use_column_width=True)
-          col2.image(new_image, caption='Processed Image', use_column_width=True)
-        else:
-          st.error('Error Status Code:{}'.format(response.status_code))
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.write("")
 
-  ### Deblur ###
-  elif choice=='Deblur':
-    if uploaded_file is not None:
-      image = Image.open(uploaded_file)
-      st.image(image, caption='Original Image', use_column_width=True)
-      st.write("")
+            # TODO: image crop canvas 수정필요
+            cropped_img = st_cropper(image, aspect_ratio=None)
 
-      #TODO: image crop
-      if st.sidebar.button('결과 보기'):
-        with st.spinner('Processing...'):
-          files = {'files':uploaded_file.getvalue()}
-          response = requests.post('http://127.0.0.1:8000/deblur',files=files) #TODO: change into server addr
-        if response.status_code==200:
-          bytes_data = io.BytesIO(response.content)
-          new_image = Image.open(bytes_data)
-          st.success('Done!')
-          st.image(new_image, caption='Processed Image', use_column_width=True)
-        else:
-          st.error('Error Status Code:{}'.format(response.status_code))
+            if st.sidebar.button('결과 보기'):
+                with st.spinner('Processing...'):
+                    image = np.array(cropped_img.convert('RGB'))
+                    img_byte_arr = ImageEncoder.Encode(image, ext='jpg', quality=90)
+                    files = {
+                        'image': img_byte_arr,
+                        'ratio': (None, int(ratio)),
+                    }
+                    response = requests.post('http://127.0.0.1:8000/super',
+                                             files=files)  # TODO: change into server addr
+                if response.status_code == 200:
+                    new_image = ImageEncoder.Decode(response.content)
+                    st.success('Done!')
+                    col1, col2 = st.columns(2)
+                    col1.image(cropped_img, caption='Cropped Image', use_column_width=True)
+                    col2.image(new_image, caption='Processed Image', use_column_width=True)
+                else:
+                    st.error('Error Status Code:{}'.format(response.status_code))
+
+    ### Deblur ###
+    elif choice == 'Deblur':
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.write("")
+            
+            canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            stroke_width=1, # drawing 두께
+            background_color="#eee", # 캔버스 바탕 색
+            background_image=Image.open(uploaded_file) if uploaded_file else None, #Pillow image to display behind canvas
+            update_streamlit=True,
+            height = int(Image.open(uploaded_file).size[1]),
+            width = int(Image.open(uploaded_file).size[0]),
+            drawing_mode="rect",
+            )
+            
+            if canvas_result.json_data is not None and uploaded_file is not None: #edit
+                height = image.size[1]
+                width = image.size[0]
+                image = np.array(image)
+                img = copy.deepcopy(image)
+                mask = np.zeros_like(image)
+
+                for ob in canvas_result.json_data["objects"]:
+                    x1,y1,x2,y2 = ob['left'], ob['top'], ob['left'] + ob['width'], ob['top'] + ob['height']
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (0), -1)
+                    cv2.rectangle(mask, (x1, y1), (x2, y2), (0), -1)   
+
+                    masked = cv2.bitwise_xor(image, img)
+
+            # TODO: test
+            if st.sidebar.button('결과 보기'):
+                with st.spinner('Processing...'):
+                    files = {'files': uploaded_file.getvalue()}
+                    response = requests.post('http://127.0.0.1:8000/deblur',
+                                             files=files)  # TODO: change into server addr
+                if response.status_code == 200:
+                    bytes_data = io.BytesIO(response.content)
+                    new_image = Image.open(bytes_data)
+                    st.success('Done!')
+                    st.image(new_image, caption='Processed Image', use_column_width=True)
+                else:
+                    st.error('Error Status Code:{}'.format(response.status_code))
+                    
+            elif st.sidebar.button('선택 영역 결과 보기'):
+                with st.spinner('Processing...'):
+                        files = {'files': masked}
+                        response = requests.post('http://127.0.0.1:8000/deblur',
+                                                 files=files)  # TODO: change into server addr
+                if response.status_code == 200:
+                    transformed_masked = np.where(masked, response, 0)
+                    transformed_image = img + transformed_masked
+                    bytes_data = io.BytesIO(transformed_image.content)
+                    new_image = Image.open(bytes_data)
+                    st.success('Done!')
+                    st.image(new_image, caption='Processed Image', use_column_width=True)
+                else:
+                    st.error('Error Status Code:{}'.format(response.status_code))
+
 
 if __name__ == '__main__':
-	main()
+    main()
