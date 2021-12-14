1edf324c194bd9586fec3f29b5e140b04bb7a4ec
jiseong.kim <intelli8786@gmail.com>
2021-12-15 01:05:32 +0900

    Streamlit Cloud 설정

--- WebServer/Service.py
+++ WebServer/Service.py
@@ -10,8 +10,10 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import PIL.Image as Image
 import requests
-sys.path.append(os.path.join(os.getcwd(), '../Utils'))
-ImageEncoder = __import__("ImageEncoder")
+from Utils import ImageEncoder
+
+#sys.path.append(os.path.join(os.getcwd(), '../Utils'))
+#ImageEncoder = __import__("ImageEncoder")
 
 # Specify canvas parameters in application
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 50, 35)
